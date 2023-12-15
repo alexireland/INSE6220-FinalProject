08b5139c64560d5b78d221d22bf12f47572a42fc
alexireland <alexireland@users.noreply.github.com>
2023-12-15 02:11:47 -0500

    updates

--- machinelearning.py
+++ machinelearning.py
@@ -1,18 +1,98 @@
 import pandas as pd
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix
 
-def run_cross_validation(file_path):
+def plot_k_value_analysis(X_train, y_train, X_test, y_test, max_k, title):
+    k_values = range(1, max_k + 1)
+    accuracies = []
+
+    for k in k_values:
+        model = KNeighborsClassifier(n_neighbors=k)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracies.append(accuracy_score(y_test, y_pred))
+
+    plt.plot(k_values, accuracies)
+    plt.title(title)
+    plt.xlabel('Number of Neighbors (k)')
+    plt.ylabel('Accuracy')
+    plt.show()
+def plot_model_accuracies(accuracies, title):
+    # Sort the models by their accuracy in descending order
+    sorted_accuracies = {k: v for k, v in sorted(accuracies.items(), key=lambda item: item[1], reverse=True)}
+
+    model_names = list(sorted_accuracies.keys())
+    model_scores = list(sorted_accuracies.values())
+
+    bars = plt.bar(model_names, model_scores, color='skyblue')
+
+    # Annotate the bars with the accuracy values
+    for bar in bars:
+        yval = bar.get_height()
+        plt.annotate(f'{yval:.3f}',  # Format to 1 decimal place
+                     (bar.get_x() + bar.get_width() / 2, yval),
+                     va='bottom', ha='center',
+                     xytext=(0, 3),  # 3 points vertical offset
+                     textcoords='offset points')
+
+    plt.xlabel('Models')
+    plt.ylabel('Accuracy')
+    plt.xticks(rotation=45)
+    plt.title(title)
+    plt.ylim(0, 1)  # Assuming accuracy is between 0 and 1
+    plt.show()
+def plot_confusion_matrix(y_true, y_pred, title):
+    cm = confusion_matrix(y_true, y_pred)
+    sns.heatmap(cm, annot=True, fmt="d", cmap='Blues')
+    plt.title(title)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.show()
+
+
+def plot_decision_boundaries(X, y, model, title):
+    # Setting min and max values and giving some padding
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                         np.arange(y_min, y_max, 0.1))
+
+    # Predict class using data and KNN classifier
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    # Plotting
+    plt.contourf(xx, yy, Z, alpha=0.4)
+    scatter = plt.scatter(X[:, 0], X[:, 1], c=y, s=20, edgecolor='k')
+
+    # Create legend
+    class_labels = np.unique(y)
+    handles = [plt.Line2D([0], [0], marker='o', color='w', label=f'Class {label}',
+                          markerfacecolor=scatter.cmap(scatter.norm(label)), markersize=10) for label in class_labels]
+    plt.legend(handles=handles)
+
+    plt.title(title)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.show()
+
+
+def run_models(file_path):
     # Load the dataset
     data = pd.read_csv(file_path)
 
     # Adjust class labels to start from 0
-    data['class'] = data['class'] - 1
+    data['class'] = data['class']  # Adjust if class labels are not starting from 0
 
     # Determine features based on the file
     features = ['PC1', 'PC2']
@@ -22,6 +102,9 @@ def run_cross_validation(file_path):
     # Splitting the data into features and target
     X = data[features]
     y = data['class']
+
+    # Splitting the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Define the models
     models = {
@@ -35,18 +118,45 @@ def run_cross_validation(file_path):
         'XGBoost': XGBClassifier(use_label_encoder=False, eval_metric='logloss')
     }
 
-    # Stratified K-Fold cross-validation
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
-    # Perform cross-validation and print results
+    # Train and evaluate each model
     for name, model in models.items():
-        print(f"Cross-validating {name} with {file_path}...")
-        scores = cross_val_score(model, X, y, cv=skf, scoring='accuracy')
-        print(f"{name} Cross-Validation Accuracy: {scores.mean():.4f} (+/- {scores.std():.4f})")
+        print(f"Training {name} with {file_path}...")
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        report = classification_report(y_test, y_pred)
+        print(f"{name} Results:")
+        print("Accuracy:", accuracy)
+        print("Classification Report:\n", report)
+
+        # Plot decision boundaries for KNN
+        if name == 'KNN':
+            plot_decision_boundaries(np.array(X), np.array(y), model, f"KNN Decision Boundary with {len(features)} PCs")
+        if name == 'KNN':
+            plot_confusion_matrix(y_test, y_pred, 'Confusion Matrix for KNN')
         print("------------------------------------------------------\n")
+        # After running models with two PCs
+        if name == 'KNN':
+            plot_k_value_analysis(X_train, y_train, X_test, y_test, 20, 'K-Value Analysis for KNN with 2 PCs')
 
-# Run cross-validation with two PCs
-run_cross_validation('PCA_output.csv')
+    accuracies = {}
+    for name, model in models.items():
+        print(f"Training {name} with {file_path}...")
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        accuracies[name] = accuracy  # Store accuracy in the dictionary
+        # ... [rest of your existing code] ...
 
-# Run cross-validation with three PCs
-run_cross_validation('PCA_output3.csv')
+    return accuracies
+
+
+# Collecting accuracies for each dataset
+accuracies_2pc = run_models('PCA_output.csv')
+# Plotting accuracies for each dataset
+plot_model_accuracies(accuracies_2pc, 'Model Accuracies with 2 Principal Components')
+# Run models with three PCs (if applicable)
+# run_models('PCA_output3.csv')
+
+
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from scipy.stats import probplot
 
 # Load dataset
 print("Loading raw dataset...")
@@ -24,23 +25,23 @@ df['BMI'] = 703 * df.iloc[:, 14] / (df.iloc[:, 1] ** 2)
 # Add an additional column based on conditional logic
 def categorize_value(val):
     if val < 25:
-        return 1
+        return 0
     elif 25 <= val <= 30:
-        return 2
+        return 1
     else:
-        return 3
+        return 2
 
 df['Class'] = df['BMI'].apply(categorize_value)
 
 
 print("Dataset has been modified and is ready for analysis.")
 
-# Box plot of the data in column 16 (index 15)
-print("Creating box plot for column 16...")
+# Box plot of the data in BMI data (index 15)
+print("Creating box plot for BMI data...")
 plt.figure(figsize=(8, 6))
 sns.boxplot(data=df.iloc[:, 15], orient='v')
-plt.title('Box Plot of BMI data')
-plt.ylabel('Column 16 values')
+plt.title('Box Plot of BMI')
+plt.ylabel('BMI')
 plt.show()
 
 # Drop the 42nd data point (row 43, index 41)
@@ -53,20 +54,27 @@ print("Loading raw dataset...")
 df = pd.read_csv('temp.csv')
 
 # Box plot of the data in column 16 (index 15) without the problematic data point
-print("Creating box plot for column 16...")
+print("Creating box plot for BMI without erronous data...")
 plt.figure(figsize=(8, 6))
 sns.boxplot(data=df.iloc[:, 15], orient='v')
-plt.title('Box Plot of Column 16')
-plt.ylabel('Column 16 values')
+plt.title('Box Plot of BMI')
+plt.ylabel('BMI')
 plt.show()
 
 # Box plot of the data in column 16 with points overlaid
-print("Creating box plot with points for column 16...")
+print("Creating box plot with points for BMI...")
 plt.figure(figsize=(8, 6))
 sns.boxplot(data=df.iloc[:, 15], orient='v', color='lightblue')
 sns.stripplot(data=df.iloc[:, 15], orient='v', color='darkblue', alpha=0.5)
-plt.title('Box Plot of Column 16 with Points')
-plt.ylabel('Column 16 values')
+plt.title('Box Plot of BMI with Data Points')
+plt.ylabel('BMI')
+plt.show()
+
+# Normal Probability Plot for the BMI column
+print("Creating normal probability plot for BMI...")
+plt.figure(figsize=(8, 6))
+probplot(df['BMI'], dist="norm", plot=plt)  # Replace 'BMI' with the actual column name if different
+plt.title('Normal Probability Plot for BMI')
 plt.show()
 
 # Correlation Graph for the first 16 columns
@@ -76,49 +84,8 @@ sns.heatmap(df.iloc[:, :16].corr(), annot=True, fmt=".2f", cmap='coolwarm')
 plt.title('Correlation Graph')
 plt.show()
 
-# Select the first 12 columns for PCA for the first pass
-print("Preparing data for first PCA...")
-df_selected = df.iloc[:, :12]
-# df_selected = df.iloc[:, 1:12]
-
-# Standardize the data
-print("Standardizing data...")
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(df_selected)
-
-# Apply PCA for 2D scatter plot
-print("Applying PCA for 2D scatter plot...")
-pca_2d = PCA(n_components=2)
-principal_components_2d = pca_2d.fit_transform(scaled_data)
-
-# 2D Scatter Plot for the first two principal components
-print("Creating 2D scatter plot...")
-plt.figure(figsize=(12, 8))
-plt.scatter(principal_components_2d[:, 0], principal_components_2d[:, 1], c=df.iloc[:, 16], cmap='viridis')
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-plt.colorbar(label='Column 17 value')
-plt.title('2D Scatter Plot of the first two Principal Components')
-plt.show()
-
-# Apply PCA for the third principal component analysis
-print("Applying PCA for PC3 analysis...")
-pca_3 = PCA(n_components=3)
-principal_components_3 = pca_3.fit_transform(scaled_data)
-
-# Scatter Plot for the third principal component and Column 17
-print("Creating scatter plot for PC3 and Column 17...")
-plt.figure(figsize=(12, 8))
-plt.scatter(principal_components_3[:, 2], range(len(principal_components_3)), c=df.iloc[:, 16], cmap='viridis', alpha=0.7)
-plt.xlabel('Principal Component 3')
-plt.ylabel('Index')
-plt.colorbar(label='Column 17 value')
-plt.title('Scatter Plot of PC3 with Colorization by Column 17')
-plt.show()
-
-# Select the first 12 columns for PCA for the SECOND pass (without age)
-print("Preparing data for first PCA...")
-# df_selected = df.iloc[:, :12]
+# Select the wanted columns for PCA
+print("Preparing data for  PCA...")
 df_selected = df.iloc[:, 1:12]
 
 # Standardize the data
@@ -137,7 +104,7 @@ plt.figure(figsize=(12, 8))
 plt.scatter(principal_components_2d[:, 0], principal_components_2d[:, 1], c=df.iloc[:, 16], cmap='viridis')
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
-plt.colorbar(label='Column 17 value')
+plt.colorbar(label='Class')
 plt.title('2D Scatter Plot of the first two Principal Components')
 plt.show()
 
@@ -152,8 +119,23 @@ plt.figure(figsize=(12, 8))
 plt.scatter(principal_components_3[:, 2], range(len(principal_components_3)), c=df.iloc[:, 16], cmap='viridis', alpha=0.7)
 plt.xlabel('Principal Component 3')
 plt.ylabel('Index')
-plt.colorbar(label='Column 17 value')
-plt.title('Scatter Plot of PC3 with Colorization by Column 17')
+plt.colorbar(label='Class')
+plt.title('Scatter Plot of PC3 with Colorization by Class')
+plt.show()
+
+# Scree Plot with Variance Explained in Percent
+print("Generating scree plot...")
+pca_all = PCA(n_components=0.999
+              )
+pca_all.fit_transform(scaled_data)
+explained_variance_percent = pca_all.explained_variance_ratio_ * 100  # Convert to percent
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, pca_all.n_components_ + 1), explained_variance_percent, 'o-', linewidth=2, color='blue')
+plt.title('Scree Plot')
+plt.xlabel('Principal Component')
+plt.ylabel('Variance Explained (%)')
+plt.xticks(range(1, pca_all.n_components_ + 1))
+plt.grid(True)
 plt.show()
 
 # Save the first two principal components and Column 17 to a CSV file
@@ -165,7 +147,39 @@ print("The first two principal components and the classification column saved to
 
 # Save the first two principal components and Column 17 to a CSV file
 print("Saving the first two principal components and the classification column to CSV file...")
-pca_df = pd.DataFrame(principal_components_3, columns=['PC1', 'PC2', 'PC2'])
+pca_df = pd.DataFrame(principal_components_3, columns=['PC1', 'PC2', 'PC3'])
 pca_df['class'] = df.iloc[:, 16]  # Add Column 17
 pca_df.to_csv('PCA_output3.csv', index=False)
 print("The first two principal components and the classification column saved to PCA_output.csv")
+
+# Grouping by the class variable and calculating the mean and standard deviation for each PC
+grouped_stats = pca_df.groupby('class')[['PC1', 'PC2', 'PC3']].agg(['median', 'std'])
+
+print("Mean and Standard Deviation of each Principal Component for each class:")
+print(grouped_stats)
+
+
+# Assuming 'grouped_stats' is your DataFrame with medians and standard deviations
+# Reset index if not already done
+grouped_stats = grouped_stats.reset_index()
+
+# Flatten the MultiIndex for ease of use
+grouped_stats.columns = [' '.join(col).strip() for col in grouped_stats.columns.values]
+
+# Reshape the DataFrame for easier plotting
+melted_stats = grouped_stats.melt(id_vars='class', value_vars=['PC1 median', 'PC1 std', 'PC2 median', 'PC2 std', 'PC3 median', 'PC3 std'])
+split_columns = melted_stats['variable'].str.split(' ', n=1, expand=True)
+melted_stats['PC'] = split_columns[0]
+melted_stats['Statistic'] = split_columns[1]
+
+# Filter out standard deviation rows for plotting
+median_stats = melted_stats[melted_stats['Statistic'] == 'median']
+
+# Create a bar plot
+plt.figure(figsize=(10, 6))
+sns.barplot(data=median_stats, x='PC', y='value', hue='class', palette='Set2')
+plt.title('Median of Principal Components by Class')
+plt.xlabel('Principal Components')
+plt.ylabel('Median Value')
+plt.legend(title='Class')
+plt.show()
